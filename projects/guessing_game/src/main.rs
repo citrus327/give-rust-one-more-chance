194f213351ed743c492b690a0f9aa6aebd1de855
citrus327 <citrus327@outlook.com>
2023-05-31 15:20:49 +0800

    feat: setup workspaces

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,5 +1,5 @@
 // https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
-use std::io;
+use std;
 
 fn main() {
     println!("Guess the number!");
@@ -7,8 +7,9 @@ fn main() {
     println!("Please input your guess.");
 
     let mut guess = String::new();
+    // let mut guess = "";
 
-    io::stdin()
+    std::io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
--- projects/syntax-test/src/main.rs
+++ projects/syntax-test/src/main.rs
@@ -1,5 +1,36 @@
+fn str_comparision() {
+    // // String creation
+    // let string_literal: &str = "Hello"; // Immutable reference to a string slice
+    // let string_owned: String = String::from("Hello"); // Owned, mutable string
+
+    // // Concatenation
+    // let concatenated: String = string_owned + " world!"; // Combine two strings
+
+    // println!("Owned string: {}", string_owned);
+    // println!("String literal: {}", string_literal);
+    // println!("Concatenated string: {}", concatenated);
+
+    // String creation
+    let string_literal: &str = "Hello"; // Immutable reference to a string slice
+    let string_owned: String = String::from("Hello"); // Owned, mutable string
+    println!("Owned string: {}", string_owned);
+
+    // Concatenation
+    let concatenated: String = string_owned + " world!"; // Combine two strings
+
+    println!("String literal: {}", string_literal);
+    println!("Concatenated string: {}", concatenated);
+}
+
 fn main() {
+    // a mutatable variable
     let mut apples = 5;
     apples = 6;
     println!("{}", apples);
+
+    // an immutable variable
+    let pears = 5;
+    println!("{}", pears);
+
+    str_comparision()
 }
