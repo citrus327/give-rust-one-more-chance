e8bc9586f12acee751123cbbe7ef4614f85f8e26
citrus327 <citrus327@outlook.com>
2023-06-01 13:59:34 +0800

    feat: complete guessing number

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -6,24 +6,33 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-    // let mut guess = "";
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("to be a number");
     let secret = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number: {secret}");
 
-    println!("You guessed: {guess}");
-    println!("The secret number: {}", secret);
+    loop {
+        println!("Please input your guess.");
 
-    match guess.cmp(&secret) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // 在loop内部重新声明guess，因为read_line会继续append输入到之前的guess中
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
